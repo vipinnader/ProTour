e80fd7de20e330ad2d56b52f9c3adb630d4b1cad
Vipin <vipin.nader@gmail.com>
2025-09-03 22:24:47 +0530

    iOS app and project creation and some other fixes.

---
+++ apps/mobile/ios/ProTour/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ProTour
+//
+//  Created by Vipin Kumar on 9/3/25.
+//
+
+#import <RCTAppDelegate.h>
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : RCTAppDelegate
+
+@end
+
---
+++ apps/mobile/ios/ProTour/AppDelegate.m
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.m
+//  ProTour
+//
+//  Created by Vipin Kumar on 9/3/25.
+//
+
+#import "AppDelegate.h"
+#import <React/RCTBundleURLProvider.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  self.moduleName = @"ProTour";
+  self.initialProps = @{};
+
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+@end
